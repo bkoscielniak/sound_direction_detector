6d8d32bca6eeffe91a76ce785ffa128d137d5963
Bartosz Kościelniak <kostek233@gmail.com>
2021-10-20 20:29:05 +0200

    added BSP drivers for display and run it
    BSP drivers were imported from STM32Cube. To ensure use this
    drivers in Zephyr (STM32 HAL and LL) were enabled suitable
    kernel options in main Kconfig for application

--- src/main.c
+++ src/main.c
@@ -7,8 +7,19 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(main);
 
+#include "stm32f769i_discovery_lcd.h"
+
 void main(void)
 {
+	uint8_t lcd_status = LCD_OK;
+
+	/* Initialize the LCD */
+        lcd_status = BSP_LCD_Init();
+        while(lcd_status != LCD_OK);
+
+        BSP_LCD_LayerDefaultInit(0, LCD_FB_START_ADDRESS);
+        BSP_LCD_Clear(LCD_COLOR_WHITE);
+
 	LOG_INF("Welcome to Sound Direction Detector, version: %u.%u.%u (%s)",
 		APP_VERSION_MAJOR, APP_VERSION_MINOR, APP_VERSION_PATCH,
 		APP_VERSION_STRING);
